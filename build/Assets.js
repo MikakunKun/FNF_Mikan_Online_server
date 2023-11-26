952317a2d683b8bb1067e7bdde15f36ec38f97af
Mikankun <120381567+MikakunKun@users.noreply.github.com>
2023-11-26 17:49:54 +0900

    remove

--- build/Assets.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Assets = void 0;
-class Assets {
-    static { this.HTML_THEME = ""; }
-}
-exports.Assets = Assets;
